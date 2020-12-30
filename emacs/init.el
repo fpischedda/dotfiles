2b59f7e03e9b23b0e2f7cb2f3209667f412c767e
Francesco Pischedda <francesco.pischedda@gmail.com>
2020-12-30 15:13:39 +0100

    [EMACS] mu4e setup

--- emacs/init.el
+++ emacs/init.el
@@ -473,6 +473,73 @@
   :config (push 'company-rtags company-backends))
 
 
+;; mu4e
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
+
+(require 'mu4e)
+
+;; use mu4e for e-mail in emacs
+(setq mail-user-agent 'mu4e-user-agent)
+
+;; the next are relative to the root maildir
+;; (see `mu info`).
+;; instead of strings, they can be functions too, see
+;; their docstring or the chapter 'Dynamic folders'
+(setq mu4e-sent-folder   "/sent"
+      mu4e-drafts-folder "/drafts"
+      mu4e-trash-folder  "/trash")
+
+;; the maildirs you use frequently; access them with 'j' ('jump')
+(setq   mu4e-maildir-shortcuts
+    '((:maildir "/archive" :key ?a)
+      (:maildir "/inbox"   :key ?i)
+      (:maildir "/work"    :key ?w)
+      (:maildir "/sent"    :key ?s)))
+
+;; the headers to show in the headers list -- a pair of a field
+;; and its width, with `nil' meaning 'unlimited'
+;; (better only use that for the last field.
+;; These are the defaults:
+(setq mu4e-headers-fields
+    '( (:date          .  25)    ;; alternatively, use :human-date
+       (:flags         .   6)
+       (:from          .  22)
+       (:subject       .  nil))) ;; alternatively, use :thread-subject
+
+;; program to get mail; alternatives are 'fetchmail', 'getmail'
+;; isync or your own shellscript. called when 'U' is pressed in
+;; main view.
+
+;; If you get your mail without an explicit command,
+;; use "true" for the command (this is the default)
+(setq mu4e-get-mail-command "mbsync -a")
+
+;; general emacs mail settings; used when composing e-mail
+;; the non-mu4e-* stuff is inherited from emacs/message-mode
+(setq mu4e-compose-reply-to-address "francesco@pischedda.info"
+      user-mail-address "francesco@pischedda.info"
+      user-full-name  "Francesco Pischedda")
+
+(setq mu4e-compose-signature
+   "Francesco Pischedda\nhttps://francesco.pischedda.info")
+
+;; smtp mail setting
+;; MAYBE LATER
+;; (setq
+;;    message-send-mail-function 'smtpmail-send-it
+;;    smtpmail-default-smtp-server "smtp.example.com"
+;;    smtpmail-smtp-server "smtp.example.com"
+;;    smtpmail-local-domain "example.com"
+
+;;    ;; if you need offline mode, set these -- and create the queue dir
+;;    ;; with 'mu mkdir', i.e.. mu mkdir /home/user/Maildir/queue
+;;    smtpmail-queue-mail  nil
+;;    smtpmail-queue-dir  "/home/user/Maildir/queue/cur")
+
+;; don't keep message buffers around
+(setq message-kill-buffer-on-exit t)
+;; end of mu4e config
+
 ;;; .emacs ends here
 
 ;;set GC to "sane" default again, it has been disabled to make startup faster
