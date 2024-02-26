04d480e5d727d1440d2149f9e09ccb98310d1844
Francesco Pischedda <francesco.pischedda@cargo.one>
2024-02-26 13:39:35 +0100

    nvchad run lazygit

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -1,13 +1,21 @@
 local M = {}
 
+M.general = {
+  n = {
+    ["<leader>gg"] = {
+      function()
+        local term = require("nvterm.terminal").new("float")
+        vim.api.nvim_chan_send(term.job_id, "lazygit\n")
+      end,
+      "open Lazygit",
+    },
+  }
+}
+
 M.conjure = {
   n = {
     ["<LocalLeader>ee"] = {"<cmd> ConjureEval <CR>", "Eval at point"},
     ["<LocalLeader>eb"] = {"<cmd> ConjureEvalBuffer <CR>", "Eval buffer"},
-  },
-  i = {
-    ["<C-a>ee"] = {"<cmd> ConjureEval <CR>", "Eval at point"},
-    ["<C-a>eb"] = {"<cmd> ConjureEvalBuffer <CR>", "Eval buffer"},
   }
 }
 
